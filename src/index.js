143915f0587d1bd0463d2f6d0817aab414443707
Imalka <kadcsd172f009@student.nibm.lk>
2021-03-05 10:17:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Avenger from "./components/avenger";
+
+
+ReactDOM.render(<Avenger/>, document.getElementById("root"))
+
